d7a9cf43e50a381912034a9d097e00a6dbeb84ea
Alex Leschenko <leschenko.al@gmail.com>
2013-02-11 03:04:35 +0200

    cleanup

--- spec/dummy/app/models/ab_admin/ab_admin_product.rb
+++ spec/dummy/app/models/ab_admin/ab_admin_product.rb
@@ -1,5 +1,24 @@
 class AbAdminProduct < AbAdmin::AbstractResource
-  preview_path :product_path
+  #preview_path :product_path
+  #preview_path { |product| product_path(product) }
+
+  #actions :except => :index
+
+  #settings :search => false, :batch => false
+
+  #batch_action(:un_publish) { |item| item.un_publish! }
+  #batch_action :destroy, false
+  #batch_action :un_publish!, :confirm => 'Un Publish?'
+
+  #action_item :destroy, false
+  #action_item :only => :show do
+  #  link_to 'Main page', '/', :class => 'btn'
+  #end
+
+  #resource_action_items :show, :edit
+  #resource_action_item do
+  #  link_to icon('arrow-down'), '/', :class => 'btn'
+  #end
 
   settings :comments => true
 
@@ -43,67 +62,5 @@ class AbAdminProduct < AbAdmin::AbstractResource
     field :picture, :as => :uploader
     field :map, :as => :map
   end
-end
 
-#class AbAdminProduct < AbAdmin::AbstractResource
-#
-#  index_view :table
-#
-#  batch_actions :destroy, :publish
-#
-#  scopes :visible, :published
-#
-#  preview :product_path
-#
-#  index_actions :edit, :show, :destroy
-#
-#  actions_on :edit => [:edit, :show, :destroy], :show => [:preview]
-#
-#  table do
-#    field :id
-#    field :sku
-#    field :name, :sortable => false
-#    image :picture
-#    field :is_visible
-#    field :created_at
-#  end
-#
-#  grid do
-#    title :name
-#    image :picture
-#    state :is_visible, :active => {:color => :green, :title => :published}, :inactive => {:color => :grey, :title => :un_published}
-#  end
-#
-#  search do
-#    field :id
-#    field :sku
-#    field :name
-#    field :is_visible
-#    field :created_at
-#  end
-#
-#  form do
-#    field :sku
-#    field :name
-#    field :description
-#    group :publish, :if => :admin? do
-#      field :is_visible
-#    end
-#    group :pictures do
-#      field :picture
-#    end
-#    has_many :modifications do
-#      field :sku
-#      field :name
-#      field :description
-#    end
-#  end
-#
-#  export do
-#    field :id
-#    field :sku
-#    field :name
-#    field :is_visible
-#    field :created_at
-#  end
-#end
+end
