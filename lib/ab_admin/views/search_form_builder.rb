cfdf007e9057127f4d99291268611b82e418a0a4
Alex Leschenko <leschenko.al@gmail.com>
2013-06-03 17:35:11 +0300

    use string_filed for text field in search form builder

--- lib/ab_admin/views/search_form_builder.rb
+++ lib/ab_admin/views/search_form_builder.rb
@@ -96,6 +96,7 @@ module AbAdmin
 
         if input_type
           return :select if options[:collection]
+          return :string if input_type == :text
         elsif @object.klass.translates? && @object.klass.translated?(attr)
           options[:value_attr] = "translations_#{attr}"
           return :string
