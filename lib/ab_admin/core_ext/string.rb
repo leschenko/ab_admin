eea541d861b729fe6b87836abe6744cd4b9b53e9
Alex Leschenko <leschenko.al@gmail.com>
2013-06-21 00:48:52 +0300

    prevent duplicate definition of lucene_escape method

--- lib/ab_admin/core_ext/string.rb
+++ lib/ab_admin/core_ext/string.rb
@@ -1,15 +1,20 @@
 # -*- encoding : utf-8 -*-
 class String
   # http://lucene.apache.org/core/old_versioned_docs/versions/2_9_1/queryparsersyntax.html
-  LUCENE_ESCAPE_REGEX = /(\+|-|&&|\|\||!|\(|\)|{|}|\[|\]|`|"|~|\?|:|\\|\/)/
+  #LUCENE_ESCAPE_REGEX = /(\+|-|&&|\|\||!|\(|\)|{|}|\[|\]|`|"|~|\?|:|\\|\/)/
   #LUCENE_ESCAPE_REGEX = /(\+|-|&&|\|\||!|\(|\)|{|}|\[|\]|`|"|~|\?|:|\\|\s)/
+
   KEYBOARDS = {
       en: 'qwertyuiop[]asdfghjkl;\'zxcvbnm,./',
       ru: 'йцукенгшщзхъфывапролджэячсмитьбю/'
   }
 
-  def lucene_escape
-    self.gsub(LUCENE_ESCAPE_REGEX, "\\\\\\1")
+  unless defined? LUCENE_ESCAPE_REGEX
+    LUCENE_ESCAPE_REGEX = /(\+|-|&&|\|\||!|\(|\)|{|}|\[|\]|\^|"|~|\*|\?|:|\\|\/)/
+
+    def lucene_escape
+      self.gsub(LUCENE_ESCAPE_REGEX, "\\\\\\1")
+    end
   end
 
   def capitalize_first
