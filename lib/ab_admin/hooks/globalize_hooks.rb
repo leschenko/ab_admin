786065c8424abafd8ed4e9a4edf885ed95bf3fd6
Alex Leschenko <leschenko.al@gmail.com>
2013-10-04 15:57:17 +0300

    switch to svenfuchs globalize3 instead of fork

---
+++ lib/ab_admin/hooks/globalize_hooks.rb
@@ -0,0 +1,26 @@
+Globalize::ActiveRecord::ClassMethods.module_eval do
+  def translations_accessor_with_locale_suffix_accessors(name)
+    translations_accessor_without_locale_suffix_accessors(name)
+
+    Globalize.available_locales.each do |locale|
+      define_method :"#{name}_#{locale}" do
+        read_attribute(name, {locale: locale})
+      end
+
+      define_method :"#{name}_#{locale}=" do |value|
+        changed_attributes[:"#{name}_#{locale}"] = value unless value == read_attribute(name, {locale: locale})
+        write_attribute(name, value, {locale: locale})
+      end
+    end
+  end
+  alias_method_chain :translations_accessor, :locale_suffix_accessors
+end
+
+module Globalize
+  mattr_accessor :available_locales
+
+  def self.valid_locale?(loc)
+    return false unless loc
+    available_locales.include?(loc.to_sym)
+  end
+end
