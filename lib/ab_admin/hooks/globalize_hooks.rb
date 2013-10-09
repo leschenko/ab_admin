5b331831c48e6baff9edca0f451e0fea5e945db5
Alex Leschenko <leschenko.al@gmail.com>
2013-10-09 11:09:14 +0300

    revert globalize hook

--- lib/ab_admin/hooks/globalize_hooks.rb
+++ lib/ab_admin/hooks/globalize_hooks.rb
@@ -1,20 +1,20 @@
-Globalize::ActiveRecord::ClassMethods.module_eval do
-  def translations_accessor_with_locale_suffix_accessors(name)
-    translations_accessor_without_locale_suffix_accessors(name)
-
-    Globalize.available_locales.each do |locale|
-      define_method :"#{name}_#{locale}" do
-        read_attribute(name, {locale: locale})
-      end
-
-      define_method :"#{name}_#{locale}=" do |value|
-        changed_attributes[:"#{name}_#{locale}"] = value unless value == read_attribute(name, {locale: locale})
-        write_attribute(name, value, {locale: locale})
-      end
-    end
-  end
-  alias_method_chain :translations_accessor, :locale_suffix_accessors
-end
+#Globalize::ActiveRecord::ClassMethods.module_eval do
+#  def translations_accessor_with_locale_suffix_accessors(name)
+#    translations_accessor_without_locale_suffix_accessors(name)
+#
+#    Globalize.available_locales.each do |locale|
+#      define_method :"#{name}_#{locale}" do
+#        read_attribute(name, {locale: locale})
+#      end
+#
+#      define_method :"#{name}_#{locale}=" do |value|
+#        changed_attributes[:"#{name}_#{locale}"] = value unless value == read_attribute(name, {locale: locale})
+#        write_attribute(name, value, {locale: locale})
+#      end
+#    end
+#  end
+#  alias_method_chain :translations_accessor, :locale_suffix_accessors
+#end
 
 module Globalize
   mattr_accessor :available_locales
