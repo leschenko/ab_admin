8dcc9306cd1a47d0bad72a1dfd24d77605c1edc0
Alex Leschenko <leschenko.al@gmail.com>
2013-10-11 13:35:50 +0300

    input_set change fieldset tag to div due to firefox bug

--- lib/ab_admin/views/admin_helpers.rb
+++ lib/ab_admin/views/admin_helpers.rb
@@ -129,7 +129,7 @@ module AbAdmin
         options.reverse_merge!(class: "inputs well well-small #{options.delete(:legend_class) || 'do_sort'}", id: options.delete(:legend_id))
         html = content_tag(:label, title, class: "input_set label #{options.delete(:label_class)}")
         html.concat(capture(&block)) if block_given?
-        content_tag(:fieldset, html, options)
+        content_tag(:div, html, options)
       end
 
       def ha(attr)
