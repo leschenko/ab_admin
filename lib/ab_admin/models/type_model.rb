6cf524f02a8b647992897cdb7b84ce39af835485
Alex Leschenko <leschenko.al@gmail.com>
2013-10-24 20:08:05 +0300

    add define_code_check_methods method to type model

--- lib/ab_admin/models/type_model.rb
+++ lib/ab_admin/models/type_model.rb
@@ -13,11 +13,35 @@ module AbAdmin
         @code = code.to_sym
       end
 
-      def self.define_enum_by_codes
-        define_enum do |builder|
-          codes.each do |kind|
-            builder.member kind, object: new(kind.to_s)
+      class << self
+        def define_enum_by_codes
+          define_enum do |builder|
+            codes.each do |kind|
+              builder.member kind, object: new(kind.to_s)
+            end
           end
+        end
+
+        def define_code_check_methods
+          codes.each do |code_check|
+            define_method "#{code_check}?" do
+              self.code == code_check
+            end
+          end
+        end
+
+        def legal?(value)
+          ActiveSupport::Deprecation.warn('legal? id deprecated, use valid? instead')
+          valid?(value)
+        end
+
+        def valid?(c_id)
+          all.map(&:id).include?(c_id.to_i)
+        end
+
+        def valid_code?(code)
+          return unless code
+          codes.include?(code.to_sym)
         end
       end
 
@@ -25,20 +49,6 @@ module AbAdmin
         I18n.t!(@code, scope: i18n_scope)
       rescue I18n::MissingTranslationData
         @code.to_s.humanize
-      end
-
-      def self.legal?(value)
-        ActiveSupport::Deprecation.warn('legal? id deprecated, use valid? instead')
-        all.map(&:id).include?(value)
-      end
-
-      def self.valid?(c_id)
-        all.map(&:id).include?(c_id.to_i)
-      end
-
-      def self.valid_code?(code)
-        return unless code
-        codes.include?(code.to_sym)
       end
 
     end
