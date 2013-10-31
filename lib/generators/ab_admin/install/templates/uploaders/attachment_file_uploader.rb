acb026d77fbbe0789a8fdb18e89695a3357fa348
Alex Leschenko <leschenko.al@gmail.com>
2013-10-31 17:44:07 +0200

    add image extensions as allowed to attachment_file_uploader

--- lib/generators/ab_admin/install/templates/uploaders/attachment_file_uploader.rb
+++ lib/generators/ab_admin/install/templates/uploaders/attachment_file_uploader.rb
@@ -1,5 +1,5 @@
 class AttachmentFileUploader < AbAdmin::CarrierWave::BaseUploader
   def extension_white_list
-    %w(pdf doc docx xls xlsx ppt pptx zip rar csv pln pla dwg dwf 3ds dgn sat psd mat max ai prg fbx dae)
+    %w(pdf doc docx xls xlsx ppt pptx jpg jpeg gif png zip rar csv pln pla dwg dwf 3ds dgn sat psd mat max ai prg fbx dae)
   end
 end
