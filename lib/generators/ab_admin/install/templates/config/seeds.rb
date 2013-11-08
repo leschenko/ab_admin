c77691d644107d1465dfae5b4e11e979a822fc00
Alex Leschenko <leschenko.al@gmail.com>
2013-11-08 05:15:50 +0200

    seed.rb params

--- lib/generators/ab_admin/install/templates/config/seeds.rb
+++ lib/generators/ab_admin/install/templates/config/seeds.rb
@@ -2,10 +2,10 @@
 
 def insert_user
   User.truncate!
-  password = Rails.env.production? ? Devise.friendly_token : (1..8).to_a.join
+  password = Rails.env.production? ? Devise.friendly_token : (1..6).to_a.join
 
   admin = User.new do |u|
-    u.email = 'test@test.com'
+    u.email = 'admin@example.com'
     u.password = password
     u.password_confirmation = password
     u.user_role_id = UserRoleType.admin.id
@@ -22,6 +22,7 @@ def insert_structures
 
   main_page = Structure.create!({title: 'Главная страница', slug: 'main-page', structure_type: StructureType.main, parent: nil}, as: :admin)
 end
+
 
 insert_user
 insert_structures
