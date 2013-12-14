8444979efe4e4e9d78bb1166249a558b0d3bd758
Alex Leschenko <leschenko.al@gmail.com>
2013-12-15 01:47:45 +0200

    css cancan more updates

--- lib/ab_admin/controllers/can_can_manager_resource.rb
+++ lib/ab_admin/controllers/can_can_manager_resource.rb
@@ -1,8 +1,9 @@
 class AbAdmin::Controllers::CanCanManagerResource < CanCan::InheritedResource
-  delegate :resource_class, to: :@controller
-
   def instance_name
     @controller.send(:resource_instance_name)
   end
 
+  def resource_class
+    @controller.send(:resource_class)
+  end
 end
