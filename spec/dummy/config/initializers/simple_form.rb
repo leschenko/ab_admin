6103abdf0688cb972587dd21fa7709a53c941fdb
Alex Leschenko <leschenko.al@gmail.com>
2013-12-15 18:12:10 +0200

    simple form update wrappers

--- spec/dummy/config/initializers/simple_form.rb
+++ spec/dummy/config/initializers/simple_form.rb
@@ -106,8 +106,11 @@ SimpleForm.setup do |config|
   # Whether attributes are required by default (or not). Default is true.
   # config.required_by_default = true
 
-  # Tell browsers whether to use default HTML5 validations (novalidate option).
-  # Default is enabled.
+  # Tell browsers whether to use the native HTML5 validations (novalidate form option).
+  # These validations are enabled in SimpleForm's internal config but disabled by default
+  # in this configuration, which is recommended due to some quirks from different browsers.
+  # To stop SimpleForm from generating the novalidate option, enabling the HTML5 validations,
+  # change this configuration to true.
   config.browser_validations = false
 
   # Collection of methods to detect if a file type was given.
@@ -128,9 +131,6 @@ SimpleForm.setup do |config|
   # Default priority for country inputs.
   # config.country_priority = nil
 
-  # Default size for text inputs.
-  # config.default_input_size = 50
-
   # When false, do not use translations for labels.
   # config.translate_labels = true
 
@@ -139,4 +139,7 @@ SimpleForm.setup do |config|
 
   # Cache SimpleForm inputs discovery
   # config.cache_discovery = !Rails.env.development?
+
+  # Default class for inputs
+  # config.input_class = nil
 end
