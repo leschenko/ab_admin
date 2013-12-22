341da77761fdd69e836b1050923a4b36708140ba
Alex Leschenko <leschenko.al@gmail.com>
2013-12-22 16:59:05 +0200

    Check for `AbAdmin.locale` in menu builder

--- lib/ab_admin/menu_builder.rb
+++ lib/ab_admin/menu_builder.rb
@@ -27,7 +27,7 @@ module AbAdmin
     include Singleton
 
     def self.draw(&block)
-      I18n.locale = AbAdmin.locale
+      I18n.locale = AbAdmin.locale if AbAdmin.locale
       instance.instance_eval &block if block_given?
     end
 
