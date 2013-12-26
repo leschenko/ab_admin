8893ae13e01cbd427c6373bc00f70385405420c9
Alex Leschenko <leschenko.al@gmail.com>
2013-12-26 16:44:43 +0200

    fix css radio

--- spec/dummy/config/initializers/ab_admin.rb
+++ spec/dummy/config/initializers/ab_admin.rb
@@ -13,3 +13,18 @@ I18n.available_locales = Globalize.available_locales = [:ru, :en]
 require 'factory_girl'
 FactoryGirl.definition_file_paths = [File.expand_path('../../factories/', __FILE__)]
 FactoryGirl.find_definitions
+
+if Rails.env.development?
+  require 'sass'
+  require 'sass/engine'
+
+  module Sass
+    class Engine
+      def initialize(template, options={})
+        @options = self.class.normalize_options(options)
+        @options[:debug_info] = true
+        @template = template
+      end
+    end
+  end
+end
