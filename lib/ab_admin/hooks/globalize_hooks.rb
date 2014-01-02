480c68bd11571b2f828012b775997cc3ade6c88f
Alex Leschenko <leschenko.al@gmail.com>
2014-01-02 03:17:25 +0200

    update globalize hooks

--- lib/ab_admin/hooks/globalize_hooks.rb
+++ lib/ab_admin/hooks/globalize_hooks.rb
@@ -8,23 +8,28 @@ module Globalize
 end
 
 Globalize::ActiveRecord::ClassMethods.module_eval do
-  def translations_accessor_with_locale_suffix_accessors(name)
-    translations_accessor_without_locale_suffix_accessors(name)
+  def define_translations_reader_with_locale_suffix(name)
+    define_translations_reader_without_locale_suffix(name)
 
     Globalize.available_locales.each do |locale|
       define_method :"#{name}_#{locale}" do
         read_attribute(name, {locale: locale})
       end
+    end
+  end
+  alias_method_chain :define_translations_reader, :locale_suffix
 
+  def define_translations_writer_with_locale_suffix(name)
+    define_translations_writer_without_locale_suffix(name)
+
+    Globalize.available_locales.each do |locale|
       define_method :"#{name}_#{locale}=" do |value|
         changed_attributes[:"#{name}_#{locale}"] = value unless value == read_attribute(name, {locale: locale})
         write_attribute(name, value, {locale: locale})
       end
     end
   end
-  alias_method_chain :translations_accessor, :locale_suffix_accessors
+  alias_method_chain :define_translations_writer, :locale_suffix
 end
 
-Globalize::ActiveRecord::Translation.class_exec do
-  attr_accessible :locale
-end
+Globalize::ActiveRecord::Translation.attr_accessible :locale
