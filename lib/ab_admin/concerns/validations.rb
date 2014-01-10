f4620e4e3b9205f30e53d2142b581b2ef2128a44
Alex Leschenko <leschenko.al@gmail.com>
2014-01-10 20:56:40 +0200

    uniq translation validation fix removed foreign_key method

--- lib/ab_admin/concerns/validations.rb
+++ lib/ab_admin/concerns/validations.rb
@@ -5,7 +5,7 @@ module AbAdmin
         def validate_each(record, attribute, value)
           ::I18n.available_locales.each do |l|
             next if record.read_attribute(attribute, locale: l).blank?
-            records_scope = record.class.const_get(:Translation).where("#{record.class.model_name.foreign_key} != #{record.id || 0}")
+            records_scope = record.class.const_get(:Translation).where("#{record.class.model_name.singular}_id != #{record.id || 0}")
             same = records_scope.where(name: record.read_attribute(attribute, locale: l), locale: l.to_s).exists?
             record.errors.add("#{attribute}_#{l}", :taken) if same
           end
