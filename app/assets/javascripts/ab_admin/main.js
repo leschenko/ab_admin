f5f9a6e2ab9fe4b6f14fcb08660c32dc0fa48f75
Alex Leschenko <leschenko.al@gmail.com>
2014-01-10 15:17:24 +0200

    css js fixes

--- app/assets/javascripts/ab_admin/main.js
+++ app/assets/javascripts/ab_admin/main.js
@@ -1,7 +1,7 @@
 //= require i18n
 //= require i18n/translations
-//= require jquery/jquery.min
-//= require jquery/jquery-ui.min
+//= require jquery.min
+//= require ab_admin/jquery-ui.min
 //= require jquery_ujs
 //= require handlebars.min
 //= require underscore.min
