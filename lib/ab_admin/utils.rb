3af9f160a3435fd5bc788d384cec02fbf9562348
Alex Leschenko <leschenko.al@gmail.com>
2014-04-18 23:55:31 +0300

    cleanup html normalizer

--- lib/ab_admin/utils.rb
+++ lib/ab_admin/utils.rb
@@ -87,12 +87,11 @@ module AbAdmin
       include ActionView::Helpers::SanitizeHelper
 
       CLEAN_HTML_COMMENTS_REGEXP = /(&lt;|<)\!--.*?--(&gt;|>)/m
-      CLEAN_COMMENTS_REGEXP = /(&lt;|<)\!--.*?--(&gt;|>)/m
       CLEAN_LINE_BREAKS_REGEXP = /[^>]\r\n/
 
       def normalize_html(raw_html)
         return '' if raw_html.blank?
-        cleaned_html = raw_html.gsub(CLEAN_HTML_COMMENTS_REGEXP, '').gsub(CLEAN_COMMENTS_REGEXP, '').gsub(CLEAN_LINE_BREAKS_REGEXP, '<br/>')
+        cleaned_html = raw_html.gsub(CLEAN_HTML_COMMENTS_REGEXP, '').gsub(CLEAN_LINE_BREAKS_REGEXP, '<br/>')
         html = sanitize(cleaned_html)
         doc = Nokogiri::HTML.fragment(html)
         #doc.xpath('comment()').each { |c| c.remove }
