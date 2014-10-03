e4124d7fdb0e94e7b0f13721cc8d9b6abe05e843
Alex Leschenko <leschenko.al@gmail.com>
2014-10-03 16:40:36 +0300

    Refactor StaticPagesController

--- app/controllers/admin/static_pages_controller.rb
+++ app/controllers/admin/static_pages_controller.rb
@@ -1,15 +1,14 @@
 class Admin::StaticPagesController < Admin::BaseController
   load_and_authorize_resource
 
-  defaults singleton: true
+  defaults singleton: true, class_name: 'Structure'
 
   belongs_to :structure, finder: :friendly_find
+
+  private
 
   def settings
     {}
   end
 
-  def parent_class
-    Structure
-  end
 end
