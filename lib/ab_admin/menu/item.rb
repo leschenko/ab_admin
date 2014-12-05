96ec67171d133823249dc374fa996b5ecd236c12
Alex Leschenko <leschenko.al@gmail.com>
2014-12-05 18:18:35 +0200

    Add badge to menu item Example:   model Payout, badge_counter: -> { 10 }, badge_type: 'info'

--- lib/ab_admin/menu/item.rb
+++ lib/ab_admin/menu/item.rb
@@ -15,9 +15,19 @@ module AbAdmin
 
         item_url = @url.is_a?(String) ? @url : call_method_or_proc_on(template, @url)
         active = template.request.path.split('/')[2] == item_url.split('/')[2]
+
         <<-HTML.html_safe
-      <li class="#{'active' if active}">#{template.link_to @title, item_url, @options.except(:if, :unless)}</li>
+          <li class="#{'active' if active}">#{template.link_to title(template), item_url, @options.except(:if, :unless)}</li>
         HTML
+      end
+
+      private
+
+      def title(template)
+        return @title unless @options[:badge_counter]
+        badge_counter = call_method_or_proc_on(template, @options[:badge_counter])
+        return @title if !badge_counter || badge_counter.zero?
+        "#{@title}&nbsp;<span class='badge badge-#{@options[:badge_type] || 'important'}'>#{badge_counter}</span>".html_safe
       end
     end
   end
