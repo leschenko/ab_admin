745b31fefcc2c6b3117813acf1a4a473029f9f93
Alex Leschenko <leschenko.al@gmail.com>
2014-12-24 16:23:38 +0200

    Cleanup ab_admin initilizer

--- spec/dummy/config/initializers/ab_admin.rb
+++ spec/dummy/config/initializers/ab_admin.rb
@@ -1,11 +1,9 @@
 require 'ab_admin/hooks'
 
-if Object.const_defined?('AbAdmin')
-  AbAdmin.setup do |config|
-    # Flash keys
-    #config.flash_keys = [ :success, :failure ]
-    config.site_name = 'Dummy'
-  end
+AbAdmin.setup do |config|
+  # Flash keys
+  #config.flash_keys = [ :success, :failure ]
+  config.site_name = 'Dummy'
 end
 
 I18n.available_locales = Globalize.available_locales = [:ru, :en, :it, :de]
