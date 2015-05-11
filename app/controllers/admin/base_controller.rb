6e3b587ddb41370d9ba3e30b0d7ec5c786e6f4c4
Alex Leschenko <leschenko.al@gmail.com>
2015-05-11 19:29:23 +0300

    Add resource singular and plural names to front vars

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -306,6 +306,10 @@ class Admin::BaseController < ::InheritedResources::Base
     fv.admin = admin?
     fv.hotkeys = settings[:hotkeys]
     fv.env = Rails.env
+    if resource_class.respond_to?(:model_name)
+      fv.resource_plural = resource_class.model_name.plural
+      fv.resource_singular = resource_class.model_name.singular
+    end
     if AbAdmin.test_env?
       fv.test = true
       AbAdmin.test_settings.each { |k, v| fv[k] = v }
