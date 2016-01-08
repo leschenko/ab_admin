9729b3c2981d8be618711630b240518886eabf89
Alex Leschenko <leschenko.al@gmail.com>
2016-01-08 12:39:56 +0200

    Cleanup

--- lib/ab_admin/core_ext.rb
+++ lib/ab_admin/core_ext.rb
@@ -1,3 +1,3 @@
 Dir["#{File.dirname(__FILE__)}/core_ext/*.rb"].sort.each do |path|
-  require "ab_admin/core_ext/#{File.basename(path, '.rb')}"
+  require "ab_admin/core_ext/#{File.basename(path)}"
 end
