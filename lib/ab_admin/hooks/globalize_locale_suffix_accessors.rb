3b1f42c255d43dc09b4e0fb5fb4fea5a510fa0cb
Alex Leschenko <leschenko.al@gmail.com>
2016-11-04 23:04:11 +0200

    Globalize locale suffix and fix

--- lib/ab_admin/hooks/globalize_locale_suffix_accessors.rb
+++ lib/ab_admin/hooks/globalize_locale_suffix_accessors.rb
@@ -1,22 +1,18 @@
 # add accessors with locale suffix like `title_en`, `title_de`
 module GlobalizeAccessorsWithLocaleSuffix
-  def define_translations_reader(name)
-    translation_attributes = class_variable_defined?(:@@translation_attributes) ? class_variable_get(:@@translation_attributes) : []
-
-    super(name)
+  def define_translated_attr_reader(name)
+    super
 
     Globalize.available_locales.each do |locale|
       method_name = "#{name}_#{locale}"
       define_method method_name.to_sym do
-        read_attribute(name, {locale: locale})
+        Globalize::Interpolation.interpolate(name, self, [locale])
       end
-      translation_attributes.push(method_name)
     end
-    class_variable_set(:@@translation_attributes, translation_attributes)
   end
 
-  def define_translations_writer(name)
-    super(name)
+  def define_translated_attr_writer(name)
+    super
 
     Globalize.available_locales.each do |locale|
       define_method :"#{name}_#{locale}=" do |value|
@@ -32,21 +28,14 @@ Globalize::ActiveRecord::ClassMethods.module_eval do
   prepend GlobalizeAccessorsWithLocaleSuffix
 end
 
-Globalize::ActiveRecord::InstanceMethods.module_eval do
-  private
-
-  # Filters translation attributes from the attribute names.
-  def attributes_for_update(attribute_names)
-    filter_translation_attributes(super)
+module GlobalizeFixResetAttribute
+  def _reset_attribute name
+    old_value = record.changed_attributes[name]
+    record.original_changed_attributes.except!(name)
+    record.send("#{name}=", old_value)
   end
+end
 
-  # Filters translation attributes from the attribute names.
-  def attributes_for_create(attribute_names)
-    filter_translation_attributes(super)
-  end
-
-  def filter_translation_attributes(attributes)
-    translation_attributes = self.class.class_variable_get(:@@translation_attributes)
-    attributes.delete_if { |attr| translation_attributes.include? attr }
-  end
+Globalize::ActiveRecord::AdapterDirty.module_eval do
+  prepend GlobalizeFixResetAttribute
 end
