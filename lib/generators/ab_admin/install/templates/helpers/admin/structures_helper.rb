53b362536a4987bad68a8a34149b36aabe59de30
Alex Leschenko <leschenko.al@gmail.com>
2017-04-28 20:03:30 +0300

    Add static page for main structure type

--- lib/generators/ab_admin/install/templates/helpers/admin/structures_helper.rb
+++ lib/generators/ab_admin/install/templates/helpers/admin/structures_helper.rb
@@ -2,15 +2,13 @@ module Admin::StructuresHelper
 
   def edit_structure_record_path(structure)
     case structure.structure_type
-      when StructureType.static_page
+      when StructureType.static_page, StructureType.main
         if structure.static_page
           edit_admin_structure_static_page_path(structure_id: structure.id)
         else
           new_admin_structure_static_page_path(structure_id: structure.id)
         end
       when StructureType.posts
-        '#'
-      when StructureType.main
         '#'
       when StructureType.redirect
         edit_admin_structure_path(structure)
@@ -20,5 +18,4 @@ module Admin::StructuresHelper
         '#'
     end
   end
-
 end
