9108973cb1c1d9e6ec93ad18f1ec4b3eb6fdb85d
Alex Leschenko <leschenko.al@gmail.com>
2019-01-16 20:56:22 +0200

    Fix globalize locale attributes

--- lib/ab_admin/hooks/globalize_locale_suffix_accessors.rb
+++ lib/ab_admin/hooks/globalize_locale_suffix_accessors.rb
@@ -2,26 +2,19 @@
 module GlobalizeAccessorsWithLocaleSuffix
   def define_translated_attr_reader(name)
     super
-
     Globalize.available_locales.each do |locale|
       method_name = "#{name}_#{locale}"
-      # attribute(method_name, ::ActiveRecord::Type::Value.new)
       define_method method_name.to_sym do
-        I18n.with_locale(locale) do
-          Globalize::Interpolation.interpolate(name, self, [locale])
-        end
+        I18n.with_locale(locale) { send(name) }
       end
     end
   end
 
   def define_translated_attr_writer(name)
     super
-
     Globalize.available_locales.each do |locale|
       define_method "#{name}_#{locale}=".to_sym do |value|
-        I18n.with_locale(locale) do
-          write_attribute(name.to_s, value, {locale: locale})
-        end
+        I18n.with_locale(locale) { send("#{name}=", value) }
       end
     end
   end
@@ -30,16 +23,3 @@ end
 Globalize::ActiveRecord::ClassMethods.module_eval do
   prepend GlobalizeAccessorsWithLocaleSuffix
 end
-
-
-# module GlobalizeFixResetAttribute
-#   def _reset_attribute name
-#     old_value = record.attribute_was(name)
-#     record.clear_attribute_changes([name])
-#     record.send("#{name}=", old_value)
-#   end
-# end
-#
-# Globalize::ActiveRecord::AdapterDirty.module_eval do
-#   prepend GlobalizeFixResetAttribute
-# end
