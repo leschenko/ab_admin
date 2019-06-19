1f8ea873acce582463098fafe4aa8740d4187db1
Alex Leschenko <leschenko.al@gmail.com>
2019-06-19 15:53:54 +0300

    Update ckeditor

--- lib/ab_admin/views/inputs/ckeditor_input.rb
+++ lib/ab_admin/views/inputs/ckeditor_input.rb
@@ -3,11 +3,11 @@ module AbAdmin
     module Inputs
       class CkeditorInput < ::SimpleForm::Inputs::Base
         def input(wrapper_options=nil)
-          unless @builder.template.instance_variable_get(:@ckeditor_init)
-            @builder.template.concat @builder.template.javascript_include_tag('/javascripts/ckeditor/init')
-            @builder.template.instance_variable_set(:@ckeditor_init, true)
+          unless @builder.template.instance_variable_get(:@ckeditor_loaded)
+            @builder.template.concat @builder.template.javascript_include_tag(Ckeditor.cdn_url)
+            @builder.template.instance_variable_set(:@ckeditor_loaded, true)
           end
-          input_html_options.reverse_merge!({width: 800, height: 200, toolbar: 'Easy'})
+          input_html_options.reverse_merge!({width: 800, height: 200})
           @builder.cktext_area(attribute_name, input_html_options)
         end
       end
