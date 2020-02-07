440fccddc686a66aa8fa24046c5b5b3c24111322
Alex Leschenko <leschenko.al@gmail.com>
2020-02-07 23:58:17 +0200

    Schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -2,11 +2,11 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
@@ -228,7 +228,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_085351) do
     t.string "trackable_type"
     t.integer "user_id"
     t.integer "owner_id"
-    t.text "trackable_changes", limit: 16777215
+    t.text "trackable_changes", size: :medium
     t.text "parameters"
     t.datetime "created_at"
     t.datetime "updated_at"
