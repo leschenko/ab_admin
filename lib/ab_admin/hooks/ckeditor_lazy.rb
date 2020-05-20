79597249872c0bc19121c34c34ba09a6fc5969b0
Alex Leschenko <leschenko.al@gmail.com>
2020-05-20 10:23:52 +0300

    Lazy load ckeditor fix

--- lib/ab_admin/hooks/ckeditor_lazy.rb
+++ lib/ab_admin/hooks/ckeditor_lazy.rb
@@ -1,9 +1,13 @@
-require 'ckeditor'
+begin
+  require 'ckeditor'
 
-Ckeditor::Utils.module_eval do
-  class << self
-    def js_replace(dom_id, options = nil)
-      options.present? ? "lazyInitCkeditor('#{dom_id}', #{ActiveSupport::JSON.encode(options)});" : "lazyInitCkEditor('#{dom_id}');"
+  Ckeditor::Utils.module_eval do
+    class << self
+      def js_replace(dom_id, options = nil)
+        options.present? ? "lazyInitCkeditor('#{dom_id}', #{ActiveSupport::JSON.encode(options)});" : "lazyInitCkEditor('#{dom_id}');"
+      end
     end
   end
+rescue LoadError => e
+  raise unless e.message.include?('ckeditor')
 end
