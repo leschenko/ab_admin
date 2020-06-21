dfa545bfd3bd5397f3188194f8df49905f32f8b8
Alex Leschenko <leschenko.al@gmail.com>
2020-06-21 23:02:26 +0300

    Webp image version naming

--- lib/ab_admin/carrierwave/base_uploader.rb
+++ lib/ab_admin/carrierwave/base_uploader.rb
@@ -25,41 +25,36 @@ module AbAdmin
 
       process :set_model_info
 
-      def full_filename(for_file=filename)
-        human_filenames ? human_full_filename(for_file) : strict_filename(for_file)
-      end
-
-      alias_method :store_filename, :filename
       def filename
-        internal_identifier || model.send("#{mounted_as}_file_name") || (store_filename && "#{secure_token}#{File.extname(store_filename).downcase}")
+        "#{[human_part, secure_token].compact.join('_')}#{extension}"
       end
 
-      def human_full_filename(for_file=filename)
-        ext = version_name.to_s.end_with?('_webp') ? '.webp' : File.extname(for_file)
-        system_part = base_filename_part
-        human_filename_part = for_file.chomp(ext)
-        return "#{system_part || version_name}#{ext}" if human_filename_part == secure_token
-        system_part ? "#{human_filename_part}_#{system_part}#{ext}" : "#{human_filename_part}#{ext}"
+      def full_filename(*)
+        return filename unless version_name
+        base = "#{version_filename_part}#{version_extension}"
+        return base unless human_filenames
+        [human_part, base].compact.join('_')
       end
 
-      def strict_filename(for_file=filename)
-        "#{version_name || secure_token}#{File.extname(for_file).downcase}"
+      def human_part
+        model.send("#{mounted_as}_file_name").to_s.remove(/\.\w+$/).remove(secure_token).chomp('_').presence
       end
 
-      def base_filename_part
-        return if version_name.to_s.remove('retina_').remove('_webp').to_sym == :default
+      def extension
+        File.extname(model.original_name).downcase
+      end
+
+      def version_extension
+        webp? ? '.webp' : extension
+      end
+
+      def version_filename_part
         return secure_token unless version_name
-        res = version_name.to_s
-        res = "#{res.remove(/_webp$/)}" if webp?
-        res = "#{res.remove(/^retina_/)}@2x" if retina?
-        res
+        strict_version_name = version_name.to_s.remove('retina_').remove('_webp')
+        strict_version_name = nil if strict_version_name.to_sym == :default
+        "#{strict_version_name}#{'@2x' if retina?}"
       end
 
-      def full_original_filename
-        "#{base_filename_part}#{File.extname(store_filename)}"
-      end
-
-      # use secure token in the filename for non processed image
       def secure_token
         model.data_secure_token ||= AbAdmin.friendly_token(20).downcase
       end
@@ -83,62 +78,38 @@ module AbAdmin
         model.original_name ||= file.original_filename if file.respond_to?(:original_filename)
       end
 
-      def write_internal_identifier(internal_identifier)
-        self.internal_identifier = internal_identifier
-        versions.values.each{|v| v.internal_identifier = internal_identifier }
-      end
-
-      # transliterate original filename
-      # allow to build custom filename
       def model_filename(base_filename, record)
         custom_file_name = model.build_filename(base_filename, record)
         return unless custom_file_name
-        normalize_filename(custom_file_name) + File.extname(base_filename).downcase
+        normalize_filename(custom_file_name)
       end
+      private :model_filename
 
       def normalize_filename(raw_filename)
-        parameterize_args = ActiveSupport::VERSION::MAJOR > 4 ? {separator: '_'} : '_'
-        I18n.transliterate(raw_filename).parameterize(**parameterize_args).gsub(/[\-_]+/, '_').downcase
+        I18n.transliterate(raw_filename).parameterize(separator: '_').gsub(/[\-_]+/, '_').downcase
       end
 
-      # rename files via move
-      def rename_via_move(new_file_name)
-        if human_filenames
-          dir = File.dirname(path)
-
-          moves = []
-          versions.values.unshift(self).each do |v|
-            from_path = File.join(dir, v.full_filename)
-            to_path = File.join(dir, v.full_filename(new_file_name))
-            next if from_path == to_path || !File.exists?(from_path)
-            moves << [from_path, to_path]
-          end
-          moves.each { |move| FileUtils.mv(*move) }
+      def rename_via_move(new_filename)
+        dir = File.dirname(path)
+        old_names = versions.values.unshift(self).map(&:full_filename)
+        model.send("#{mounted_as}_file_name=", "#{[new_filename.presence, secure_token].compact.join('_')}#{extension}")
+        new_names = versions.values.unshift(self).map(&:full_filename)
+        old_names.zip(new_names).each do |old_name, new_name|
+          old_path, new_path = File.join(dir, old_name), File.join(dir, new_name)
+          next if old_path == new_path || !File.exists?(old_path)
+          FileUtils.mv(old_path, new_path)
         end
-
-        write_internal_identifier new_file_name
-        model.send("write_#{mounted_as}_identifier")
-        retrieve_from_store!(new_file_name) if human_filenames
-
-        new_file_name
+        retrieve_from_store!(model.send("#{mounted_as}_file_name"))
       end
 
-      private :write_internal_identifier, :store_filename, :model_filename
-
-      def rmagick_included?
-        self.class.included_modules.map(&:to_s).include?('CarrierWave::RMagick')
-      end
-
-      # prevent large number of subdirectories
       def store_dir
         str_id = model.id.to_s.rjust(4, '0')
         [AbAdmin.uploads_dir, model.class.to_s.underscore, str_id[0..2], str_id[3..-1]].join('/')
       end
 
       def convert_to_webp(options = {})
-        webp_path = "#{File.dirname(path)}/#{filename.sub(/\.\w+$/, '.webp')}"
+        webp_path = "#{File.dirname(path)}/#{full_filename}"
         WebP.encode(path, webp_path, options_for_webp(options))
-        write_internal_identifier webp_path.split('/').pop
         @file = ::CarrierWave::SanitizedFile.new(tempfile: webp_path, filename: webp_path, content_type: 'image/webp')
       end
 
@@ -207,7 +178,7 @@ module AbAdmin
 
         unless degrees.blank?
           manipulate! do |img|
-            rmagick_included? ? img.rotate!(degrees.to_i) : img.rotate(degrees.to_s)
+            self.class.included_modules.map(&:to_s).include?('CarrierWave::RMagick') ? img.rotate!(degrees.to_i) : img.rotate(degrees.to_s)
             img = yield(img) if block_given?
             img
           end
