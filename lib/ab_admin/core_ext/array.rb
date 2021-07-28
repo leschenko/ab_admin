2e583c47a5954c30490c517c25c8e5105960d0c4
Alex Leschenko <leschenko.al@gmail.com>
2021-07-28 21:59:32 +0300

    Cleanup

--- lib/ab_admin/core_ext/array.rb
+++ lib/ab_admin/core_ext/array.rb
@@ -1,9 +1,4 @@
 class Array
-  # TOREMOVE after ruby 2.7.0
-  def tally
-    each_with_object(Hash.new(0)) { |v, h| h[v] += 1 }
-  end
-
   def deep_merge_hashes
     self.inject({}) do |res, h|
       raise Exception.new("Not a hash #{h}") unless h.is_a?(Hash)
