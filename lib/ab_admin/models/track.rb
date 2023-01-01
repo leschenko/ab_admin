0fe94834a64ae183c0e046b08913d53f0e4af2b1
Alex Leschenko <leschenko.al@gmail.com>
2023-01-01 22:12:37 +0200

    Force convert trackable_changes to Hash

--- lib/ab_admin/models/track.rb
+++ lib/ab_admin/models/track.rb
@@ -63,7 +63,7 @@ module AbAdmin
 
       def make_trackable
         self.name ||= trackable.han.first(250)
-        self.trackable_changes = trackable.saved_changes.except(:updated_at)
+        self.trackable_changes = trackable.saved_changes.except(:updated_at).to_h
       end
     end
   end
