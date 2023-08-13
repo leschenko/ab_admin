e48f2a0ad2807d6c7789febfdc306d538697326f
Alex Leschenko <leschenko.al@gmail.com>
2023-08-13 16:05:57 +0300

    Revert whitelisting ransack attributes

--- spec/dummy/app/models/application_record.rb
+++ spec/dummy/app/models/application_record.rb
@@ -1,3 +1,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.inherited(subclass)
+    super
+    subclass.singleton_class.class_eval do
+      def ransackable_attributes(*)
+        @@ransackable_attributes ||= authorizable_ransackable_attributes
+      end
+
+      def ransackable_associations(*)
+        @@ransackable_associations ||= authorizable_ransackable_associations
+      end
+    end
+  end
 end
