7c5ddd74c8d4d55b60e0809983a2e84ea0b4475c
Alex Le <581415+leschenko@users.noreply.github.com>
2026-02-23 15:24:59 +0100

    Fix serializable attributes in Track

--- lib/ab_admin/models/track.rb
+++ lib/ab_admin/models/track.rb
@@ -9,8 +9,8 @@ module AbAdmin
         belongs_to :owner, class_name: 'User'
         belongs_to :user, class_name: 'User'
 
-        serialize :parameters, type: Hash
-        serialize :trackable_changes, type: Hash
+        serialize :parameters, coder: YAML, type: Hash
+        serialize :trackable_changes, coder: YAML, type: Hash
 
         before_create :make_trackable, if: :trackable
 
